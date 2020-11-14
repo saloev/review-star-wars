4fd960cd69941822dc49d7b9574861227559a197
saloev <saloev.saadi@yandex.ru>
2020-11-15 02:13:20 +0300

    feat: film list from API

--- src/js/view/FilmList.js
+++ src/js/view/FilmList.js
@@ -10,13 +10,36 @@ export default class BaseLayout extends BaseView {
     this.filmList = this.getFromLS("list");
   }
 
+  filmTemplate({
+    title,
+    episode_id: id,
+    director,
+    producer,
+    opening_crawl: desc,
+    release_date: releaseDate,
+  }) {
+    return `<div class="film">
+      <header class="film__header">
+        <h3 class="film__title">${title}</h3>
+        <p class="film__director">${director}</p>
+        <p class="film__producer">${producer}</p>
+      </header>
+      <p class="film__dec">${desc}</p>
+      <time datetime="${releaseDate}" class="film__release-date">${releaseDate}</time>
+    </div>`;
+  }
+
   init = () => {
-    this.render();
+    this.renderHTML();
     this.initEvents();
   };
 
-  render() {
-    const list = this.filmList;    
+  renderHTML() {
+    const list = this.filmList.reduce(
+      (html, film) => `${html}<li>${this.filmTemplate(film)}</li>`,
+      ""
+    );
+    super.render(`<ul>${list}</ul>`);
   }
 
   initEvents = () => {};
